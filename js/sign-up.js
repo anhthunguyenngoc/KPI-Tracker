26ced263cf0b7bb4f3bc0f289110e3d5bbc6db4f
anhthunguyenngoc <anhthunguyenngoc80@gmail.com>
2024-05-14 20:04:44 +0700

    feat : update sign up & add sign in

---
+++ js/sign-up.js
@@ -0,0 +1,8 @@
+document.getElementById("sign_in").addEventListener("click", function() {
+    window.open("../html/sign-in.html", '_top');
+});
+
+//Trong trang ddkii sau khi ng dùng đăng ký thành công thì sẽ hiện hdsd
+document.getElementById("sign_up").addEventListener("click", function() {
+    window.open("#", '_top');
+});
