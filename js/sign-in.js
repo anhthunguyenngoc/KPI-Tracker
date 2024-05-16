2ac3b8a88cf51e72fc3a85edf97249a0a83b08ff
anhthunguyenngoc <anhthunguyenngoc80@gmail.com>
2024-05-16 21:18:16 +0700

    feat : update frame style, font size

--- js/sign-in.js
+++ js/sign-in.js
@@ -2,6 +2,10 @@ document.getElementById("sign_in").addEventListener("click", function() {
     window.open("../html/trang-chu.html", '_top');
 });
 
+document.getElementById("sign_in_2").addEventListener("click", function() {
+    window.open("../html/trang-chu.html", '_top');
+});
+
 document.getElementById("sign_up").addEventListener("click", function() {
     window.open("../html/sign-up.html", '_top');
 });
@@ -9,3 +13,24 @@ document.getElementById("sign_up").addEventListener("click", function() {
 document.getElementById("index").addEventListener("click", function() {
     window.open("../index.html", '_top');
 });
+
+document.getElementById("forgot_pass").addEventListener("click", function() {
+    document.getElementById("sign-in-frame").style.display = "none";
+    document.getElementById("forgot_pass-1-frame").style.display = "flex";
+});
+
+document.getElementById("forgot_pass-button").addEventListener("click", function() {
+    document.getElementById("forgot_pass-1-frame").style.display = "none";
+    document.getElementById("forgot_pass-2-frame").style.display = "flex";
+});
+
+document.getElementById("sign-in-back").addEventListener("click", function() {
+    document.getElementById("sign-in-frame").style.display = "flex";
+    document.getElementById("forgot_pass-1-frame").style.display = "none";
+});
+
+document.getElementById("forgot_pass-1-back").addEventListener("click", function() {
+    document.getElementById("forgot_pass-1-frame").style.display = "flex";
+    document.getElementById("forgot_pass-2-frame").style.display = "none";
+});
+
